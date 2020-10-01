baa17e772571d5f7104297273600a6e2d3ad2dd8
Muhammad Fajariansyah Ismail <fajariansyah.ismail@gmail.com>
2020-10-01 11:08:15 +0700

    Update 1 October 2020

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d7530cd97cc4f9669f285e64b316229.js"
+  "/facility-timeline/precache-manifest.0e5a8f21502312c1bad8e9565268f366.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/facility-timeline/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
